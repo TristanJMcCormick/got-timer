dbcd48d9a09c14a52450765c1fcdc28778eb22d7
Tristan McCormick <tristanjmccormick@gmail.com>
2019-04-15 21:08:23 -0400

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,57 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Game of Thrones gif" src="./assets/success-logo.gif" />
+    <HelloGot :secondsTillGotStarts="secondsTillGotStarts" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloGot from "./components/HelloGot.vue";
+import setDay from "date-fns/set_day";
+import differenceInMilliseconds from "date-fns/difference_in_milliseconds";
 
 export default {
-  name: 'app',
+  name: "app",
+  data: function() {
+    return {
+      secondsTillGotStarts: 0
+    };
+  },
+  methods: {
+    calculateGotTimer: function() {
+      const today = new Date();
+      const nextGotStarts = new Date(
+        Date.UTC(
+          today.getFullYear(),
+          today.getMonth(),
+          setDay(today, 7).getDate() + 1,
+          1,
+          0,
+          0
+        )
+      );
+      this.secondsTillGotStarts =
+        -differenceInMilliseconds(today, nextGotStarts) / 1000;
+      setTimeout(this.calculateGotTimer, 1000);
+    }
+  },
+  created: function() {
+    this.calculateGotTimer();
+  },
   components: {
-    HelloWorld
+    HelloGot
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: white;
   margin-top: 60px;
+  background-color: black;
 }
 </style>
---
+++ src/components/HelloGot.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="hello">
+    <h1>
+      {{
+        `Game of Thrones is on in ${timerObject[0]} days, ${
+          timerObject[1]
+        } hours, ${timerObject[2]} minutes, and ${timerObject[3]} seconds`
+      }}
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloGot",
+  props: {
+    secondsTillGotStarts: Number
+  },
+  computed: {
+    timerObject: function() {
+      const daysTillGot = Math.floor(
+        this.secondsTillGotStarts / (60 * 60 * 24)
+      );
+      const hoursInRemainder = Math.floor(
+        (this.secondsTillGotStarts % (60 * 60 * 24)) / (60 * 60)
+      );
+      const minutesInRemainder = Math.floor(
+        (this.secondsTillGotStarts % (60 * 60)) / 60
+      );
+      const secondsInRemainder = Math.floor(this.secondsTillGotStarts % 60);
+      const timerObject = [
+        daysTillGot, // days
+        hoursInRemainder,
+        minutesInRemainder, // minutes
+        secondsInRemainder // seconds
+      ];
+      return timerObject;
+    }
+  }
+};
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
